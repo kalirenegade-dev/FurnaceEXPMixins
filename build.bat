d78f79d218407084e9e537e8af9a7f0f23205fa4
Kali <michael040990@gmail.com>
2024-02-12 00:47:49 -0500

    build file

---
+++ build.bat
@@ -0,0 +1,30 @@
+@echo off
+timeout /t 2 /nobreak > nul
+
+set "jarPath=build\libs\FurnaceEXPMixins-2.0.0.jar"
+set "destinationPath=U:\instances\RLCraft293HardcoreSponge01\Minecraft\mods\FurnaceEXPMixins-2.0.0.jar"
+
+echo Deleting "%jarPath%"
+del "%jarPath%" 2>nul || echo File not found or unable to delete.
+
+echo Building "%jarPath%"
+call gradlew.bat build --no-daemon
+if errorlevel 1 (
+    echo Build failed.
+    pause
+    exit /b 1
+)
+
+echo Deploying "%jarPath%" to "%destinationPath%"
+copy /Y "%jarPath%" "%destinationPath%" || (
+    echo Copy failed.
+    pause
+    exit /b 1
+)
+
+echo Deployment successful.
+
+echo restarting server.
+
+java -jar Rlcraft_Restart.jar
+pause
